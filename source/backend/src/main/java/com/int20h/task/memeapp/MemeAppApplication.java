83782b1121de7d1984f6895bc7ad14f0b1cb4ae6
Valeriy Bryliov <valychbreak@gmail.com>
2018-01-22 23:21:12 +0200

    Initial commit

---
+++ source/backend/src/main/java/com/int20h/task/memeapp/MemeAppApplication.java
@@ -0,0 +1,12 @@
+package com.int20h.task.memeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeAppApplication.class, args);
+	}
+}
